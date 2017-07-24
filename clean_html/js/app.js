d8d64b8a2e009141be7454a3fed99aa714f3d957
Kamadzy <kam4513@gmail.com>
2017-07-25 02:51:36 +0300

    added freewal and some main content

--- clean_html/js/app.js
+++ clean_html/js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-    $("body").niceScroll({
+
+    $("body, .left_side").niceScroll({
         horizrailenabled: false
     });
 
@@ -8,5 +9,31 @@ $(document).ready(function () {
         $(".left_side").toggleClass("active");
     });
 
+    var wall = new freewall(".gallery");
+    wall.reset({
+        selector: "a",
+        animate: true,
+        cellW: 150,
+        cellH: "auto",
+        gutterX : 5,
+        gutterY : 5,
+        onResize: function() {
+            wall.fitWidth();
+        }
+    });
 
+    var images = wall.container.find("a");
+    images.find("img").on('load', function() {
+        wall.fitWidth();
+    });
+
+// $(".filter-label").click(function() {
+// 	$(".filter-label").removeClass("active");
+// 	var filter = $(this).addClass("active").data("filter");
+// 	wall.filter(filter);
+// 	setTimeout(function() {
+// 		$(window).resize();
+// 		wall.fitWidth();
+// 	}, 400);
+// });
 });
