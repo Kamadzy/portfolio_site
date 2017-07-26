f96b4a5f20c1586b5543741762ca12e2913ce402
Kamadzy <kam4513@gmail.com>
2017-07-26 20:16:01 +0300

    added filter for images

--- clean_html/js/app.js
+++ clean_html/js/app.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
 
     $("body, .left_side").niceScroll({
-        horizrailenabled: false
+        horizrailenabled : false
     });
 
-    $(".toggle_menu").click(function() {
+    $(".toggle_menu").click(function () {
         $(".sandwich").toggleClass("active");
         $(".left_side").toggleClass("active");
     });
@@ -15,25 +15,36 @@ $(document).ready(function () {
         animate: true,
         cellW: 150,
         cellH: "auto",
-        gutterX : 5,
-        gutterY : 5,
-        onResize: function() {
+        gutterX: 5,
+        gutterY: 5,
+        onResize: function () {
             wall.fitWidth();
         }
     });
 
     var images = wall.container.find("a");
-    images.find("img").on('load', function() {
+    images.find("img").on('load', function () {
         wall.fitWidth();
     });
 
-// $(".filter-label").click(function() {
-// 	$(".filter-label").removeClass("active");
-// 	var filter = $(this).addClass("active").data("filter");
-// 	wall.filter(filter);
-// 	setTimeout(function() {
-// 		$(window).resize();
-// 		wall.fitWidth();
-// 	}, 400);
-// });
+    $(".filter_label").click(function () {
+        $(".filter_label").removeClass("active");
+        var filter = $(this).addClass("active").data("filter");
+        wall.filter(filter);
+        setTimeout(function () {
+            $(window).resize();
+            wall.fitWidth();
+        }, 400);
+    });
+
+    $(".gallery img").lazyload({
+        effect: "fadeIn",
+        threshold: 1000
+    }).parent().hover(function () {
+        $(".gallery a").css("opacity", ".6");
+        $(this).css("opacity", "1");
+    }, function () {
+        $(".gallery a").css("opacity", "1");
+    });
+
 });
